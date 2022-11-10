b54a3610da157b6980e2d10b1f9436482a4e10e4
ptlharshil <patelharshil3695@gmail.com>
2022-11-10 18:43:51 -0500

    changes for deployment

--- fe/src/pages/Tweets/Home.js
+++ fe/src/pages/Tweets/Home.js
@@ -12,7 +12,7 @@ const THome = () => {
       //const res = await fetch("http://localhost:5000/tweets-results");
       //const res = await fetch(`${process.env.REACT_APP_BE_NETWORK}:${process.env.REACT_APP_BE_PORT}/tweets-results`);
       //const res = await fetch(`${process.env.REACT_APP_API_SERVICE_URL}/tweets-results`);
-      const res = await fetch(`tweets-results`);
+      const res = await fetch(`${process.env.REACT_APP_API_SERVICE_URL}/tweets-results`);
       const { results } = await res.json();
       console.log(results);
       setTweets([...results]);
